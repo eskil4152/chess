12e6a34536e8934775c699860c1932e6a54caa91
eskil <eskil@blikeng.com>
2026-04-28 21:35:25 +0200

    Init

---
+++ src/test/java/com/blikeng/chess/ChessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blikeng.chess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
