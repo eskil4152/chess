6af5dd0e2d54cdd9ed56d5fc5a4e6969ec8e019b
eskil <eskil@blikeng.com>
2026-04-29 15:21:03 +0200

    Piece update
    * Added square under attack detection and inCheck detection

--- src/main/java/com/blikeng/chess/engine/MoveExecutor.java
+++ src/main/java/com/blikeng/chess/engine/MoveExecutor.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 public class MoveExecutor {
     private final MoveGenerator moveGenerator = new MoveGenerator();
+    private final SquareAttacked squareAttacked = new SquareAttacked();
 
     public boolean performMove(GameEntity game, Move move) {
         Board board = game.getBoard();
---
+++ src/main/java/com/blikeng/chess/engine/SquareAttacked.java
@@ -0,0 +1,37 @@
+package com.blikeng.chess.engine;
+
+import com.blikeng.chess.entity.GameEntity;
+import com.blikeng.chess.model.Board;
+import com.blikeng.chess.model.Position;
+import com.blikeng.chess.model.piece.Color;
+import com.blikeng.chess.model.piece.Piece;
+
+import java.util.List;
+
+public class SquareAttacked {
+    private final MoveGenerator moveGenerator = new MoveGenerator();
+
+    public boolean isInCheck(GameEntity game, Color color){
+        Position kingPos = color == Color.WHITE
+                ? game.getWhiteKingPosition()
+                : game.getBlackKingPosition();
+
+        Color attacker = color == Color.WHITE ? Color.BLACK : Color.WHITE;
+        return isSquareAttacked(game.getBoard(), kingPos, attacker);
+    }
+
+    private boolean isSquareAttacked (Board board, Position position, Color attackingColor){
+        for (int row = 0; row < 8; row++){
+            for (int col = 0; col < 7; col++){
+                Piece piece = board.getPiece(row, col);
+
+                if (piece == null || piece.getColor() != attackingColor) return false;
+
+                List<Position> moves = moveGenerator.getLegalMoves(board, new Position(row, col));
+                if (moves.contains(position)) return true;
+            }
+        }
+
+        return false;
+    }
+}
