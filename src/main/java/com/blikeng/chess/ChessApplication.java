648c08fe74db219d82a9df1f181270ef0bb46d8b
eskil <eskil@blikeng.com>
2026-05-13 00:52:17 +0200

    Pong
    - Attempting scheduled server-side pinging

--- src/main/java/com/blikeng/chess/ChessApplication.java
+++ src/main/java/com/blikeng/chess/ChessApplication.java
@@ -2,8 +2,10 @@ package com.blikeng.chess;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ChessApplication {
 	static void main(String[] args) {
 		SpringApplication.run(ChessApplication.class, args);
