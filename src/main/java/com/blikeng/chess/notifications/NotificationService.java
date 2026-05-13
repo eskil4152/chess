32cdc41e6695397a2b828a740db564683c66f909
eskil <eskil@blikeng.com>
2026-05-13 11:41:08 +0200

    Beefy ping revert
    - Revert recent edits. Error was caused by Cloudflare, not application. Provider changed
    - Keeping server-side ping and session removal

--- src/main/java/com/blikeng/chess/notifications/NotificationService.java
+++ src/main/java/com/blikeng/chess/notifications/NotificationService.java
@@ -16,6 +16,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
+import org.springframework.web.socket.PingMessage;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 
@@ -78,12 +79,13 @@ public class NotificationService {
 
     @Scheduled(fixedRate = 20000)
     public void pingAllSessions() {
+        PingMessage ping = new PingMessage();
         presenceService.getAllSessions().forEach(session -> {
             ReentrantLock lock = sessionLocks.computeIfAbsent(session.getId(), _ -> new ReentrantLock());
             lock.lock();
             try {
                 if (session.isOpen()) {
-                    session.sendMessage(new TextMessage("{\"type\":\"PING\"}"));
+                    session.sendMessage(ping);
                 } else {
                     UUID userId = (UUID) session.getAttributes().get("userId");
                     presenceService.removeSession(userId, session);
