ff1d7a74ddab464e58f97816c58cba04fb46178c
eskil <eskil@blikeng.com>
2026-05-19 14:18:32 +0200

    Patch
    - Override equal and hashCode for FriendId
    - Remove old handleTransportError. Caused noise on client DC
    - Re-expose actuator with only health endpoint
    - Exclude unused UserDetailsServiceAutoConfiguration

--- src/main/java/com/blikeng/chess/ChessApplication.java
+++ src/main/java/com/blikeng/chess/ChessApplication.java
@@ -2,9 +2,10 @@ package com.blikeng.chess;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
 @EnableScheduling
 public class ChessApplication {
 	static void main(String[] args) {
