93c1d80ffb894361d1ea8998161287b5037eee29
eskil <eskil@blikeng.com>
2026-06-02 16:04:16 +0200

    Wins
    - Add wins and win percentage to profile
    - Add win percentage calculation method

--- src/main/java/com/blikeng/chess/dto/ProfileDTO.java
+++ src/main/java/com/blikeng/chess/dto/ProfileDTO.java
@@ -4,14 +4,27 @@ public record ProfileDTO (
         String username,
         String bio,
         String avatarUrl,
+
         int bulletElo,
         int bulletGames,
+        int bulletWins,
+        double bulletWinPercentage,
+
         int blitzElo,
         int blitzGames,
+        int blitzWins,
+        double blitzWinPercentage,
+
         int rapidElo,
         int rapidGames,
+        int rapidWins,
+        double rapidWinPercentage,
+
         int classicalElo,
         int classicalGames,
+        int classicalWins,
+        double classicalWinPercentage,
+
         boolean isFriend,
         String activeGameId
 ){
