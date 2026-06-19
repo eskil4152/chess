961d6ec8a9edda6499182072390dfdaa85cc9374
eskil <eskil@blikeng.com>
2026-06-19 18:09:08 +0200

    Redis init
    - Added redis to pom.xml
    - Configured redis config

---
+++ src/main/java/com/blikeng/chess/config/RedisConfig.java
@@ -0,0 +1,16 @@
+package com.blikeng.chess.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+
+@Configuration
+public class RedisConfig {
+    @Bean
+    public RedisMessageListenerContainer redisMessageListenerContainer(RedisConnectionFactory factory) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(factory);
+        return container;
+    }
+}
